cae06613fcc3021379d8d416a1731889bb692baa
Prakash8782 <prakash8782@gmail.com>
2022-09-10 17:11:30 +0530

    operator

---
+++ main.js
@@ -0,0 +1,52 @@
+// Assignment operator
+let a=7;
+a++;
+console.log(a);
+// console.log(++a);
+
+// arithmetic operator
+let b=45;
+let c=18;
+console.log(b+c);
+console.log(b-c);
+console.log(b*c);
+console.log(b/c);
+console.log(b%c);
+
+// multiplication operator
+
+let x=5;
+let y=10;
+let m=x*y;
+console.log(m);
+
+var p = 7;
+p+= 5;
+console.log(p);
+
+let i=2;
+let j=7;
+let k=i**j;
+console.log(k);
+
+let fname="Prakash";
+let lname="Praveen";
+let fullname=fname+" "+lname;
+console.log(fullname);
+
+let quotes = "If Your  Are Bad";
+quotes += " I Am Your Dad";
+console.log(quotes);
+
+
+let e = 3 + 4;
+console.log(e);
+let f = "5" + 2;
+console.log(f);
+let g = "Virat " + 18;
+console.log(g);
+
+let w=7;
+let v=18;
+console.log(w>v? v:w);
+
